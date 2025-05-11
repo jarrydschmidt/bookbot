8353d82ddf9814d2eeb61eec873d265e6c5c9c3c
jarrydschmidt <jarrydschmidt@gmail.com>
2025-05-11 01:03:31 -0700

    added sort_on and get_sorted_list functions to stats and added a report block to main calling stats functions

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ def get_book_text(filepath):
 # handles text inputs
 from stats import get_num_words
 from stats import get_num_characters
+from stats import get_sorted_list
 
 def main(): # prints word count and character count
-    book_text = get_book_text("books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+    book_text = get_book_text(path)
+    word_count = get_num_words(book_text)
     char_count = get_num_characters(book_text)
+    sorted_chars = get_sorted_list(char_count)
     
-    print(f'{get_num_words(book_text)} words found in the document')   
-    print(char_count)  
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,15 @@ def get_num_characters(text): # obtains unique character count after converting 
         else:
             char_count[char] = 1
     return char_count
+
+def sort_on(dict): 
+    return dict["num"] 
+# This is a gap in my learning path, see: "Python functions as first-class objects" or "higher-order functions in Python"
+
+def get_sorted_list(char_dict):
+    result_list = []
+    for char, count in char_dict.items(): # tuple unpacking, adding each to a new char list
+        new_char_dict = {"char": char, "num": count}
+        result_list.append(new_char_dict)
+    result_list.sort(reverse=True, key=sort_on)
+    return result_list
