13c84e414dbe426efd5a6293cb89570461731122
jarrydschmidt <jarrydschmidt@gmail.com>
2025-05-12 16:36:02 -0700

    added sys.argv to handle std in forcustom filepath

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-    return file_contents
-# handles text inputs
+
+import sys 
 from stats import get_num_words
 from stats import get_num_characters
 from stats import get_sorted_list
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+def get_book_text(filepath): # handles text inputs
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
 def main(): # prints word count and character count
-    path = "books/frankenstein.txt"
+    path = sys.argv[1] 
     book_text = get_book_text(path)
     word_count = get_num_words(book_text)
     char_count = get_num_characters(book_text)
